ce9ba5cb0bc891aa3124a7c67f910ca21c2a6cca
Jes Schultz <jes.schultz@gmail.com>
2020-11-11 15:49:00 -0700

    Fixed mdf to bak in push_image

--- container/Docker/create_docker_image.sh
+++ container/Docker/create_docker_image.sh
@@ -2,4 +2,9 @@
 
 docker build -t mssql-bak-bacpac .
 
-docker run --name bak-to-bacpac mssql-bak-bacpac
+#docker run --name bak-to-bacpac mssql-bak-bacpac
+
+# An SA_PASSWORD must be enetered here 
+docker run -e "ACCEPT_EULA=Y" -e "SA_PASSWORD=" `
+-v C:/Docker/mdf:/mnt/external `
+--name bak-to-bacpac mssql-bak-bacpac
