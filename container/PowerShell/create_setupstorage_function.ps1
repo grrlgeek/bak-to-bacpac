b776240150233b7b5b6714b6b60861a6e40d926d
SQLDbaWithABeard <mrrobsewell@outlook.com>
2020-11-13 17:37:27 +0000

    Naming convention for functions and splat

--- container/PowerShell/create_setupstorage_function.ps1
+++ container/PowerShell/create_setupstorage_function.ps1
@@ -1,60 +1,61 @@
 # Create function to create storage account and file share, and generate key 
 
-function SetupStorage {
+function New-ContainerStorageSetUp {
     param( [string]$StorageResourceGroupName, 
-           [string]$StorageAccountName, 
-           [string]$ShareName,
-           [string]$Location)
+        [string]$StorageAccountName, 
+        [string]$ShareName,
+        [string]$Location)
 
     # check if storage account exists
-    $StorageAccount = Get-AzStorageAccount `
-        -ResourceGroupName $StorageResourceGroupName `
-        -Name $StorageAccountName `
-        -ErrorAction SilentlyContinue
 
-    if ($StorageAccount -eq $null) {
+    $StorageAcctParams = @{
+        ResourceGroupName = $StorageResourceGroupName
+        Name              = $StorageAccountName
+        ErrorAction       = 'SilentlyContinue'
+    }
+    $StorageAccount = Get-AzStorageAccount @StorageAcctParams
+
+    if ($null -eq $StorageAccount) {
         # create the storage account
-        $StorageAccount = New-AzStorageAccount `
-            -ResourceGroupName $StorageResourceGroupName `
-            -Name $StorageAccountName `
-            -SkuName Standard_LRS `
-            -Location $Location
+        $StorageAccount = New-AzStorageAccount @StorageAcctParams -Location $Location -SkuName Standard_LRS
     }
 
     # check if the file share already exists
-    $Share = Get-AzStorageShare `
-        -Name $ShareName -Context $StorageAccount.Context `
-        -ErrorAction SilentlyContinue
 
-    if ($Share -eq $null) {
+    $AzShareParams = @{
+        Name        = $ShareName
+        Context     = $StorageAccount.Context
+        ErrorAction = 'SilentlyContinue'
+    }
+
+    $Share = Get-AzStorageShare @AzShareParams
+
+    if ($null -eq $Share) {
         # create the share
-        $Share = New-AzStorageShare `
-            -Name $ShareName `
-            -Context $StorageAccount.Context
+        $Share = New-AzStorageShare @AzShareParams
     }
 
     <# Add a container for blob storage also, same name #>
     # check if the file share already exists
-    $Container = Get-AzStorageContainer `
-    -Name $ShareName -Context $StorageAccount.Context `
-    -ErrorAction SilentlyContinue
 
-    if ($Container -eq $null) {
+    $AzStorageContainerParams = @{
+        Name        = $ShareName
+        Context     = $StorageAccount.Context
+        ErrorAction = 'SilentlyContinue'
+    }
+    $Container = Get-AzStorageContainer @AzStorageContainerParams
+
+    if ($null -eq $Container) {
         # create the container
-        $Container = New-AzStorageContainer `
-            -Name $ShareName `
-            -Context $StorageAccount.Context
+        $Container = New-AzStorageContainer @AzStorageContainerParams
     }
     <# End blob storage #>
 
     # get the credentials
-    $StorageAccountKeys = Get-AzStorageAccountKey `
-        -ResourceGroupName $StorageResourceGroupName `
-        -Name $StorageAccountName
-
+    $StorageAccountKeys = Get-AzStorageAccountKey -ResourceGroupName $StorageResourceGroupName -Name $StorageAccountName
     $StorageAccountKey = $StorageAccountKeys[0].Value
     $StorageAccountKeySecureString = ConvertTo-SecureString $StorageAccountKey -AsPlainText -Force
     $StorageAccountCredentials = New-Object System.Management.Automation.PSCredential ($StorageAccountName, $StorageAccountKeySecureString)
-    
-    return $StorageAccountCredentials
+
+    $StorageAccountCredentials
 }
