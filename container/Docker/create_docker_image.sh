7d645961e447714453dd1b1f3eed81d4b4b3895c
SQLDbaWithABeard <mrrobsewell@outlook.com>
2020-11-13 17:34:49 +0000

    so that it would run for me

--- container/Docker/create_docker_image.sh
+++ container/Docker/create_docker_image.sh
@@ -1,10 +1,9 @@
-# Create Docker image locally from Dockerfile 
-
 docker build -t mssql-bak-bacpac .
 
-#docker run --name bak-to-bacpac mssql-bak-bacpac
+# docker run --name bak-to-bacpac mssql-bak-bacpac
 
-# An SA_PASSWORD must be enetered here 
-docker run -e "ACCEPT_EULA=Y" -e "SA_PASSWORD=" `
--v C:/Docker/mdf:/mnt/external `
---name bak-to-bacpac mssql-bak-bacpac
+# An SA_PASSWORD must be entered here and 
+# and the directory where the bak files are on your PC
+# If running in WSL use the format /mnt/DRIVELETTER/Directory
+
+ docker run -e "ACCEPT_EULA=Y" -e "SA_PASSWORD=Password0!" -v /mnt/f/BackupShare:/mnt/external  --name bak-to-bacpac mssql-bak-bacpac
