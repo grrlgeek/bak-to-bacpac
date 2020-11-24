524975e32b7c84cac874e4cc218aa80459aa09b5
Rob Sewell <mrrobsewell@outlook.com>
2020-11-24 21:44:41 +0000

    so that it is more obvious

--- container/PowerShell/00-Create-Local-Docker-Container.ps1
+++ container/PowerShell/00-Create-Local-Docker-Container.ps1
@@ -5,11 +5,22 @@
 
 # Make sure your prompt is at the root of the repository and run.
 
-. ./container/PowerShell/variables.ps1
+. ./container/PowerShell/variables.ps1 
 
 ## To create a local container image and run it
 # This will load any .bak files in the localDockerHostDirectory
 
+<#
+# to create the create_docker_image.sh file with the details filled in use this code
 (Get-Content ./container/Docker/create_docker_image.sh) -replace '__SA_PASSWORD__', $containerSaPassword.GetNetworkCredential().Password -replace '__LOCAL_HOST_DIRECTORY__',$localDockerHostDirectory | Set-Content $dockertempcreateimage
 
-Get-Content $dockertempcreateimage
+#>
+Set-Location ./container/Docker
+
+
+#build the image
+docker build -t mssql-bak-bacpac .
+
+$localdockerrun = ('docker run -e "ACCEPT_EULA=Y" -e "SA_PASSWORD=__SA_PASSWORD__" -v __LOCAL_HOST_DIRECTORY__:/mnt/external  --name bak-to-bacpac mssql-bak-bacpac' -replace '__SA_PASSWORD__', $containerSaPassword.GetNetworkCredential().Password -replace '__LOCAL_HOST_DIRECTORY__',$localDockerHostDirectory)
+
+Invoke-Expression $localdockerrun
