aaea0aae5f733ffcc791a5270e567f48b5b6fc7a
Rob Sewell <mrrobsewell@outlook.com>
2020-11-24 18:24:05 +0000

    so that we can test locally

--- container/Docker/local_docker_run_command.sh
+++ container/Docker/local_docker_run_command.sh
@@ -2,4 +2,4 @@
 # An SA_PASSWORD must be entered here 
 # and the directory where the bak files are on your PC
 # If running in WSL use the format /mnt/DRIVELETTER/Directory
-docker run -e "ACCEPT_EULA=Y" -e "SA_PASSWORD=Password0!" -v /mnt/f/BackupShare:/mnt/external --name bak-to-bacpac mssql-bak-bacpac
+docker run -e "ACCEPT_EULA=Y" -e "SA_PASSWORD=Password0!" -v __LOCAL_HOST_DIRECTORY__:/mnt/external --name bak-to-bacpac mssql-bak-bacpac
