5633f90767c86c3fe91cbbae6247205c505c4e29
Rob Sewell <mrrobsewell@outlook.com>
2020-11-24 18:23:25 +0000

    so that we dont have passwords in clear text

--- container/PowerShell/variables.ps1
+++ container/PowerShell/variables.ps1
@@ -1,3 +1,29 @@
+#region local docker image variables
+
+# this is the password for the sa account for the container
+if(-not ($containerSaPassword)){
+    $containerSaPassword = Get-Credential -Message "Enter a password for the sa account for the container"
+}
+
+# Set the temp area so this works x-plat
+
+$dockertempdir = [system.io.path]::GetTempPath() + 'docker/'
+if(-not(Test-Path $dockertempdir)){
+    New-Item $dockertempdir -ItemType Directory
+}
+$dockertempcreateimage = "$dockertempdir\create_docker_image.sh"
+
+
+
+# This is the path on the host to the directory for testing the image and container
+# If you are on Windows and using WSL use the format /mnt/DRIVELETTER/Directory
+# If you are using Windows use DRIVELETTER:\PathtoDirectory
+
+$localDockerHostDirectory = '/mnt/f/BackupShare'
+
+#endregion
+
+
 $RGName = 'sqlcontainers' 
 $Location = 'northeurope'
 $KVName = 'beardkvsqlcontainers' # must be unique across Azure
@@ -18,3 +44,4 @@ $VolumeMountPath  = '/mnt/external'
 $SQLDB = 'importedbak'
 $sqlEdition = 'BusinessCritical'
 $sqlSLO = 'BC_Gen5_2'
+
