80643ad4da30fc99cae51aff4af3bedac8691f57
SQLDbaWithABeard <mrrobsewell@outlook.com>
2020-11-25 07:58:24 +0000

    so the file has more info in it

--- container/PowerShell/00-Create-Local-Docker-Container.ps1
+++ container/PowerShell/00-Create-Local-Docker-Container.ps1
@@ -1,26 +1,33 @@
-# A Script to run all of the required scripts in order
-# A beginning step to modulisation I guess
+<# 
+A Script to create the container image and then to run a test if you wish (or for demos)
 
+This will create an image called mssql-bak-bacpac and using the values set in the local 
+docker region of the variables file run the container and process any .bak files that you 
+have placed in the directory referenced in $localDockerHostDirectory
+
+#>
 # First Set the variables in the variables.ps1 file to the required values
+# This will provide a credential prompt, the user is not important, the password will
+# be the SA password for the container
 
 # Make sure your prompt is at the root of the repository and run.
 
 . ./container/PowerShell/variables.ps1 
 
-## To create a local container image and run it
-# This will load any .bak files in the localDockerHostDirectory
+# This will build the image and name it mssql-bak-bacpac
 
-<#
-# to create the create_docker_image.sh file with the details filled in use this code
-(Get-Content ./container/Docker/create_docker_image.sh) -replace '__SA_PASSWORD__', $containerSaPassword.GetNetworkCredential().Password -replace '__LOCAL_HOST_DIRECTORY__',$localDockerHostDirectory | Set-Content $dockertempcreateimage
-
-#>
 Set-Location ./container/Docker
 
-
-#build the image
 docker build -t mssql-bak-bacpac .
+
+# This will create a local container named bak-to-bacpac from the mssql-bak-bacpac image and run it
+# This will process any .bak files in the localDockerHostDirectory
 
 $localdockerrun = ('docker run -e "ACCEPT_EULA=Y" -e "SA_PASSWORD=__SA_PASSWORD__" -v __LOCAL_HOST_DIRECTORY__:/mnt/external  --name bak-to-bacpac mssql-bak-bacpac' -replace '__SA_PASSWORD__', $containerSaPassword.GetNetworkCredential().Password -replace '__LOCAL_HOST_DIRECTORY__',$localDockerHostDirectory)
 
 Invoke-Expression $localdockerrun
+
+<#
+To remove the container
+docker container rm bak-to-bacpac
+#>
